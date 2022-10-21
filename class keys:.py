0e92c0a1d224e684fb48dfaea6615e5ec48f0b52
chrischerian <ccherian@wharton.upenn.edu>
2022-10-21 03:10:01 -0400

    Remove unnecessary files
    nuked old project files

--- class keys:.py
+++
@@ -1,8 +0,0 @@
-class keys:
-
-    def __init__(self, color, double, triple):
-        self.color = color
-        self.double = double
-        self.triple = triple
-    
-print("\033[32m time")
--- class_keyboard.py
+++
@@ -1,8 +0,0 @@
-class keyboard:
-
-    line1 = "Q  W  E  R  T  Y  U  I  O  P"
-    line2 = " A  S  D  F  G  H  J  K  L"
-    line3 = "   Z  X  C  V  B  N  M"
-
-    
-
